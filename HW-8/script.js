a391cd38e597e6cd3ac09a8897305bc3261d919d
Danielle Chard <42789596+Daniellechard@users.noreply.github.com>
2020-03-09 14:35:05 -0600

    hw8 finish

--- HW-8/script.js
+++ HW-8/script.js
@@ -35,46 +35,46 @@
   $(document).ready(function() {
 
       $("button").click(function(){
-          $(imageArray[getRandomIndex()].theImageSelector).attr("src", imageArray[getRandomIndex()].theImagePath).fadeIn("slow").fadeOut("slow");
+          $(imageArray[getRandomIndex()].theImageSelector).attr("src", imageArray[getRandomIndex()].theImagePath).fadeIn("slow").fadeOut(4000);
  $("#l5").toggle();
       });
   });
 
   function moveText() {
       for (var i = 0; i < 20; i++){
-      $("#l1").fadeOut("slow").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+      $("#l1").fadeOut(5000).fadeIn(35000);
     }
      for (var j = 1; j < 20; j ++){
-      $("#l2").fadeOut("slow").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+      $("#l2").fadeOut(10000).fadeIn(40000);
      }
      for (var k = 1; k < 20; k ++){
-      $("#l3").fadeOut("slow").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+      $("#l3").fadeOut(15000).fadeIn(45000);
      }
      for (var l = 1; l < 20; l ++){
-      $("#l4").fadeOut("slow").fadeIn("slow").fadeOut("slow").fadeIn("slow");
+      $("#l4").fadeOut(20000).fadeIn(50000);
      }
 
 
 
   }
   function moveSquare(){
-      for (var i = 0; i < 4; i ++) {
-        // purple
-          $("#square0").animate({top:185}).animate({left:450}).fadeOut().fadeIn().animate({top:750}).animate({left:1200}).fadeOut().fadeIn();
+      for (var i = 0; i < 4; i ++)
+      {
+          $("#square0").animate({top:300}).animate({left:600}).fadeOut().fadeIn().animate({top:1000}).animate({left:1500}).fadeOut().fadeIn();
       }
 
-      for (var j = 0; j < 4; j ++) {
-        // green
-          $("#square1").animate({left:200}).animate({top:800}).fadeOut().fadeIn().animate({left:1400}).animate({top:110}).fadeOut().fadeIn();
+      for (var j = 0; j < 4; j ++)
+      {
+          $("#square1").animate({top:300}).animate({left:500}).fadeOut().fadeIn().animate({top:1000}).animate({left:1400}).fadeOut().fadeIn();
       }
 
-      for (var k = 0; k < 5; k ++) {
-        // pink
-      $("#square2").animate({left:450}).fadeOut().fadeIn().animate({left:1200}).fadeOut().fadeIn();
+     for (var k = 0; k < 5; k ++)
+     {
+          $("#square2").animate({top:300}).animate({left:400}).fadeOut().fadeIn().animate({top:1000}).animate({left:1300}).fadeOut().fadeIn();
       }
 
-      for (var l = 0; l < 5; l ++) {
-        // pink
-      $("#square3").animate({left:650}).animate({top:650}).fadeOut().fadeIn().animate({left:1200}).fadeOut().fadeIn();
-      }
+    for (var l = 0; l < 5; l ++)
+    {
+     $("#square3").animate({top:300}).animate({left:300}).fadeOut().fadeIn().animate({top:1000}).animate({left:1200}).fadeOut().fadeIn();
+    }
   }
