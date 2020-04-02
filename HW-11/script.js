32e03168c93044abd71a0f4190a73136a012da00
Danielle Chard <42789596+Daniellechard@users.noreply.github.com>
2020-04-01 18:24:27 -0600

    update walls

--- HW-11/script.js
+++ HW-11/script.js
@@ -8,11 +8,13 @@ var direction;
 var questions;
 var squareArray = [];
 var lives = 1;
-$(document).ready(function(){
+$(document).ready(function()
+{
 
     setup();
 
-    $(this).keypress(function(event){
+    $(this).keypress(function(event)
+    {
         getKey(event);
 
     });
@@ -29,16 +31,17 @@ function setup()
     square1 = new Square(50,50,25,25,"rgb(0, 255, 232)");
 
     //pink
-    stuff1 = new Square(850, 445, 10,10,"rgb(255, 0, 199)");
+    stuff1 = new Square(600, 300, 10,10,"rgb(255, 0, 199)");
     //yellow
-	  stuff2 = new Square(700,650,10,10,"rgb(250, 255, 0)");
+	  stuff2 = new Square(300,600,10,10,"rgb(250, 255, 0)");
     //red
-    stuff3 = new Square(500,445,10,10,"rgb(255, 0, 0)");
+    stuff3 = new Square(700,800,10,10,"rgb(255, 0, 0)");
 
 
 
 
-    $.getJSON("data.json", function(data) {
+    $.getJSON("data.json", function(data)
+    {
         for(var i = 0; i < data.squares.length; i++)
         {
             squareArray.push(new Square(data.squares[i].x,data.squares[i].y, data.squares[i].h, data.squares[i].w, data.squares[i].color));
@@ -75,13 +78,14 @@ function getKey(event)
         direction = "right";
     }
 
-// added new stuff under here
+
 
 var check = false;
-for(var i = 0; i < squareArray.length; i++){
-
+for(var i = 0; i < squareArray.length; i++)
+{
     check = hasCollided(square1,squareArray[i]);
-    if(check === true){
+    if(check === true)
+    {
         break;
     }
 }
@@ -89,21 +93,24 @@ for(var i = 0; i < squareArray.length; i++){
 
 var stuffOne = false;
     stuffOne = hasCollided(square1,stuff1);
-    if(stuffOne === true){
+    if(stuffOne === true)
+    {
   lives++;
   stuff1 = new Square(0, 0, 0,0,"rgb(71, 71, 219)");
     }
 
 var stuffTwo = false;
     stuffTwo = hasCollided(square1,stuff3);
-    if(stuffTwo === true){
+    if(stuffTwo === true)
+    {
   lives++;
   stuff3 = new Square(0, 0, 0,0,"rgb(50, 252, 174)");
     }
 
 var stuffThree = false;
     stuffThree = hasCollided(square1,stuff2);
-    if(stuffThree === true){
+    if(stuffThree === true)
+    {
   lives++;
   stuff2 = new Square(0, 0, 0,0,"rgb(207, 17, 96)");
     }
@@ -111,7 +118,8 @@ var stuffThree = false;
 
 
 
-if(check){
+if(check)
+{
     if(direction === "left"){moveRight();}
     else if(direction === "right"){moveLeft();}
     else if(direction === "up"){moveDown();}
@@ -143,18 +151,18 @@ function moveLeft()
 
 function drawSquare()
 {
-    ctx.clearRect(0,0,1400,800);
+    ctx.clearRect(0,0,1000,1000);
+
     ctx.fillStyle = square1.mainColor;
     ctx.fillRect(square1.x, square1.y, square1.width, square1.height);
 
-
     ctx.fillStyle = stuff1.mainColor;
-        ctx.fillRect(stuff1.x, stuff1.y, stuff1.width, stuff1.height);
+    ctx.fillRect(stuff1.x, stuff1.y, stuff1.width, stuff1.height);
 
-    	ctx.fillStyle = stuff2.mainColor;
-        ctx.fillRect(stuff2.x, stuff2.y, stuff2.width, stuff2.height);
+    ctx.fillStyle = stuff2.mainColor;
+    ctx.fillRect(stuff2.x, stuff2.y, stuff2.width, stuff2.height);
 
-    	ctx.fillStyle = stuff3.mainColor;
+    ctx.fillStyle = stuff3.mainColor;
     ctx.fillRect(stuff3.x, stuff3.y, stuff3.width, stuff3.height);
 
 
