a597c805fc303d84978f04764df97b79ec2fa858
Danielle Chard <42789596+Daniellechard@users.noreply.github.com>
2020-05-08 12:43:49 -0600

    finished!

--- Portfolio Final/main.js
+++ Portfolio Final/main.js
@@ -3,4 +3,14 @@ $('.menu-toggler').on('click', function() {
   $(this).toggleClass('open');
   $('.top-nav').toggleClass('open');
   });
+
+  $('.top-nav .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1500,
+  });
 });
